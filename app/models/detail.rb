3ada1841caaecb46d506ec67f76932fcc5de3f3a
maekawa <kazumaekawa7218@gmail.com>
2020-10-13 23:03:03 +0900

    detailモデルにて外部キーなしでも保存できるよう設定

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -1,5 +1,5 @@
 class Detail < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :area
   belongs_to_active_hash :occupation
