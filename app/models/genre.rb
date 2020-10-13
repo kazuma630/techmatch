4130b847b749dedc73f6cb42950fcb58202a0be4
maekawa <kazumaekawa7218@gmail.com>
2020-10-13 22:38:52 +0900

    ActiveHashの導入・モデル編集

---
+++ app/models/genre.rb
@@ -0,0 +1,10 @@
+class Genre < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--'},
+    { id: 2, name: '自社開発'},
+    { id: 3, name: 'SES'},
+    { id: 4, name: '受託開発(Sler)'},
+    { id: 5, name: 'フリーランス'},
+    { id: 6, name: '経営者'}
+  ]
+end
