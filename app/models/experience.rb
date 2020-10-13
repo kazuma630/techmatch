9e5c2dbff41d485c7253d41ea48daccb00fa2fbb
maekawa <kazumaekawa7218@gmail.com>
2020-10-13 22:38:52 +0900

    ActiveHashの導入・モデル編集

---
+++ app/models/experience.rb
@@ -0,0 +1,11 @@
+class Experience < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '完全未経験' },
+    { id: 3, name: '独学中' },
+    { id: 4, name: 'スクール生' },
+    { id: 5, name: '実務歴1年未満' },
+    { id: 6, name: '実務歴1~3年' },
+    { id: 7, name: '実務歴3年以上' }
+  ]
+end
---
+++ app/models/language.rb
@@ -0,0 +1,25 @@
+class Language < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: 'HTML/CSS' },
+    { id: 3, name: 'JavaScript' },
+    { id: 4, name: 'Ruby/Rails' },
+    { id: 5, name: 'PHP' },
+    { id: 6, name: 'Python' },
+    { id: 7, name: 'SQL' },
+    { id: 8, name: 'R' },
+    { id: 9, name: 'Java' },
+    { id: 10, name: 'C/C++' },
+    { id: 11, name: 'C#' },
+    { id: 12, name: 'VBA' },
+    { id: 13, name: 'Swift' },
+    { id: 14, name: 'Go' },
+    { id: 15, name: 'Perl' },
+    { id: 16, name: 'Visual Basic' },
+    { id: 17, name: 'Unity' },
+    { id: 18, name: 'Kotlin' },
+    { id: 19, name: 'Scala' },
+    { id: 20, name: 'TypeScript' },
+    { id: 21, name: 'Bash/Shell' }
+  ]
+end
