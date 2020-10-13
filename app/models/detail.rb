f6f32a74fb5a5dbfc19fdddb204e32cd990d8f5f
maekawa <kazumaekawa7218@gmail.com>
2020-10-13 22:58:59 +0900

    detailモデルのバリデーションを設定

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -1,2 +1,20 @@
 class Detail < ApplicationRecord
+  belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :area
+  belongs_to_active_hash :occupation
+  belongs_to_active_hash :genre
+  belongs_to_active_hash :experience
+  belongs_to_active_hash :language
+  belongs_to_active_hash :interest
+
+  with_options presence: true do
+    validates :age
+    validates :pr
+    with_options numericality: { other_than: 1} do
+      validates :area
+      validates :occupation
+      validates :interest
+    end
+  end
 end
