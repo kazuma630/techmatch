f920cb3d6706a76ce0536e2452bcbe2afdd1b464
maekawa <kazumaekawa7218@gmail.com>
2020-10-19 10:38:07 +0900

    ユーザー編集機能の導入(途中)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,4 +30,38 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     current_user.stop_following(@user)
   end
+
+  # def edit
+  #   @user = User.find(params[:id])
+  # end
+
+  # def update
+  #   @user = User.new(sign_up_params)
+  #   unless @user.valid?
+  #     render :new and return
+  #   end
+  #   session["devise.regist_data"] = {user: @user.attributes}
+  #   session["devise.regist_data"][:user]["password"] = params[:user][:password]
+  #   @detail = @user.build_detail
+  #   render :new_detail
+  # end
+
+  # def update_detail
+  #   @user = User.new(session["devise.regist_data"]["user"])
+  #   @detail = Detail.new(detail_params)
+  #     unless @detail.valid?
+  #       render :new_detail and return
+  #     end
+  #   @user.build_detail(@detail.attributes)
+  #   @user.update
+  #   session["devise.regist_data"]["user"].clear
+  #   redirect_to user_path(params[id])
+  # end
+
+  private
+
+  # def detail_params
+  #   params.require(:detail).permit(:age, :area_id, :occupation_id, :genre_id, :school, :experience_id, :language_id, :interest_id, :pr)
+  # end
+
 end
