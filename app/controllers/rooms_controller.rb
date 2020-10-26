3fac881ac5ae108c5ac6df8c8ea9fc9e301a1c8c
maekawa <kazumaekawa7218@gmail.com>
2020-10-26 17:54:37 +0900

    roomsコントローラーの編集(途中)

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -9,10 +9,13 @@ class RoomsController < ApplicationController
   end
 
   def show
+    @users = current_user.followings && current_user.followers
     @room = Room.find(params[:id])
     if Entry.where(user_id: current_user.id, room_id: @room.id).present?
-      # @rooms = Room.where(user_id: current_user.id)
-      # @entries_full = Entry.where(room_id: @rooms.room_id)
+      @rooms = Entry.where(user_id: current_user.id)
+      binding.pry
+      @entries_full = Entry.where(room_id: @rooms.room_id)
+      
       @messages = @room.messages
       @message = Message.new
       @entries = @room.entries
