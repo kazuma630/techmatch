8381cc80e1c3f26220b925d6df27c9320b2796e2
maekawa <kazumaekawa7218@gmail.com>
2020-10-28 19:17:20 +0900

    チャットページのレイアウト修正

--- app/javascript/packs/chat.js
+++ app/javascript/packs/chat.js
@@ -1,26 +1,41 @@
-window.addEventListener('load', () => {
+// $(document).on('turbolinks:load', function() {
+// window.addEventListener('load', () => {
+  function load(){
+
+
   const chatButton = document.getElementById("video-button")
-  const videoEnd = document.getElementById("video-end")
+  
 
 chatButton.addEventListener('click', () => {
   document.getElementById("chat").style.display = 'block';
   document.getElementById("message").style.height = 'calc(100vh - 538.5px)';
 })
+}
+// })
+setTimeout(load, 1000)
 
-videoEnd.addEventListener('click', () => {
+function down(){
+  const videoEnd = document.getElementById("video-end")
+  videoEnd.addEventListener('click', () => {
   location.reload();
 })
-})
+}
 
+setInterval(down, 1000)
 
 import Vue from 'vue/dist/vue.esm'
 import Chat from '../chat.vue'
 
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
+  function loading(){
+
+
   const app = new Vue({
     el: '#chat',
     data: {
     },
     components: { Chat }
   })
-})
+}
+// })
+setTimeout(loading, 1000)
