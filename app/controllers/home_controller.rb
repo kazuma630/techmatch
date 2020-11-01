82b017976e17f76b2dc82fb5e0a0750affa3172d
maekawa <kazumaekawa7218@gmail.com>
2020-11-01 23:49:39 +0900

    ページネーションの定義とログイン確認

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,17 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!
+  before_action :login_required
 
   def index
     @follow_users = current_user.all_following
     @follower_users = current_user.followers
-    # @users_mutual = @follow_users && @follower_users
+    @follower_users = Kaminari.paginate_array(@follower_users).page(params[:page]).per(5)
+  end
+
+  private
+
+  def login_required
+    unless user_signed_in?
+      redirect_to tops_index_path
+    end
   end
 end
