3481e2b3603a885167b6ab71fa3e16e519482ada
maekawa <kazumaekawa7218@gmail.com>
2020-11-15 17:26:21 +0900

    検索関連のリファクタリング

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -3,13 +3,14 @@ class Interest < ActiveHash::Base
     { id: 1, name: '指定なし' },
     { id: 2, name: '就職活動' },
     { id: 3, name: '転職活動' },
-    { id: 4, name: '副業' },
-    { id: 5, name: '独立' },
-    { id: 6, name: 'ノマドワーカー' },
+    { id: 4, name: 'デザイン' },
+    { id: 5, name: '情報発信' },
+    { id: 6, name: '共同アプリ開発' },
     { id: 7, name: 'プログラミングスクール' },
-    { id: 8, name: '勉強会' },
-    { id: 9, name: '食事会' },
-    { id: 10, name: '飲み会' },
-    { id: 11, name: '技術交流' }
+    { id: 8, name: 'もくもく会' },
+    { id: 9, name: 'オフ会' },
+    { id: 10, name: 'サーバサイド系' },
+    { id: 11, name: 'フロントエンド系' }.
+    { id: 12, name: 'インフラ系' }
   ]
 end
