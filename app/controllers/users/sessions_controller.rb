8489dbce1a7d0382becfbad0a7d1c9ccfe000a6f
maekawa <kazumaekawa7218@gmail.com>
2020-11-15 21:33:39 +0900

    デプロイエラー修正(sessions_controller)

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,7 +4,7 @@ class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   def new_guest
-    User.find_or_create_by!(email: 'guest@example.com') do |user|
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
       user.name = "太郎"
     end
